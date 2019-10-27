9ca5caadec1a4d246f3aa641d3fbe990f42b82d6
Dilip Poudel <dilippoudel@Dilip-MacBook-Pro.local>
2019-10-27 17:07:53 +0200

    error fixed

--- tootle/src/components/GoogleAuth.js
+++ tootle/src/components/GoogleAuth.js
@@ -11,7 +11,6 @@ class GoogleAuth extends React.Component {
     render() { 
         return ( 
             <div>
-                  <GoogleAuth/>
               GoogleAuth
             </div>
              );
